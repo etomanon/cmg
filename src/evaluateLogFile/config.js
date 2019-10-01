385128eb125288d85d67264cd69e6625e66a21f0
Jan <etomanon@gmail.com>
2019-10-01 21:49:32 +0200

    add header index

--- src/evaluateLogFile/config.js
+++ src/evaluateLogFile/config.js
@@ -1,31 +1,40 @@
 const { mean, std } = require("mathjs");
 
 const config = {
-  thermometer: (referenceValue, values) => {
-    const meanValues = mean(values);
-    const stdValues = std(values);
-    const isMeanWithin = Math.abs(meanValues - referenceValue) <= 0.5;
-    if (isMeanWithin && stdValues < 3) {
-      return "ultra precise";
+  thermometer: {
+    headerIndex: 1,
+    validate: (referenceValue, values) => {
+      const meanValues = mean(values);
+      const stdValues = std(values);
+      const isMeanWithin = Math.abs(meanValues - referenceValue) <= 0.5;
+      if (isMeanWithin && stdValues < 3) {
+        return "ultra precise";
+      }
+      if (isMeanWithin && stdValues < 5) {
+        return "very precise";
+      }
+      return "precise";
     }
-    if (isMeanWithin && stdValues < 5) {
-      return "very precise";
-    }
-    return "precise";
   },
-  humidity: (referenceValue, values) => {
-    const isOutsideLimit = values.some(v => Math.abs(v - referenceValue) > 1);
-    if (isOutsideLimit) {
-      return "discard";
+  humidity: {
+    headerIndex: 2,
+    validate: (referenceValue, values) => {
+      const isOutsideLimit = values.some(v => Math.abs(v - referenceValue) > 1);
+      if (isOutsideLimit) {
+        return "discard";
+      }
+      return "keep";
     }
-    return "keep";
   },
-  monoxide: (referenceValue, values) => {
-    const isOutsideLimit = values.some(v => Math.abs(v - referenceValue) > 3);
-    if (isOutsideLimit) {
-      return "discard";
+  monoxide: {
+    headerIndex: 3,
+    validate: (referenceValue, values) => {
+      const isOutsideLimit = values.some(v => Math.abs(v - referenceValue) > 3);
+      if (isOutsideLimit) {
+        return "discard";
+      }
+      return "keep";
     }
-    return "keep";
   }
 };
 
