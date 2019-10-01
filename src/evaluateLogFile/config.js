636d12c231a717ccfda08c9fda69c85c1ca955c4
Jan <etomanon@gmail.com>
2019-10-01 20:49:49 +0200

    add humidity and monoxide config

--- src/evaluateLogFile/config.js
+++ src/evaluateLogFile/config.js
@@ -1,17 +1,32 @@
 const { mean, std } = require("mathjs");
-const 
 
 const config = {
   thermometer: (referenceValue, values) => {
     const meanValues = mean(values);
     const stdValues = std(values);
-    const isMeanWithin = Math.abs(meanValues - referenceValue) < 0.5;
-    if(isMeanWithin  && stdValues < 3) {
+    const isMeanWithin = Math.abs(meanValues - referenceValue) <= 0.5;
+    if (isMeanWithin && stdValues < 3) {
       return "ultra precise";
     }
-    if(isMeanWithin && stdValues < 5) {
+    if (isMeanWithin && stdValues < 5) {
       return "very precise";
     }
-    return "precise"
+    return "precise";
+  },
+  humidity: (referenceValue, values) => {
+    const isOutsideLimit = values.some(v => Math.abs(v - referenceValue) > 1);
+    if (isOutsideLimit) {
+      return "discard";
+    }
+    return "keep";
+  },
+  monoxide: (referenceValue, values) => {
+    const isOutsideLimit = values.some(v => Math.abs(v - referenceValue) > 3);
+    if (isOutsideLimit) {
+      return "discard";
+    }
+    return "keep";
   }
-}
+};
+
+module.exports = config;
